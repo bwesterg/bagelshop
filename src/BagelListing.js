c03cb7e7024dba9f1f58add144f9a410f8368ef1
bwesterg <benton.westergaard@gmail.com>
2021-07-12 16:50:33 -0600

    Stopping point reached.  Still need to finish add function, also fix new bagel function.

--- src/BagelListing.js
+++ src/BagelListing.js
@@ -5,11 +5,26 @@ export default function BagelListing(props) {
     const deleteBagel = event => {
         props.deleteBagel(props.id)
     }
+
+    const editBagel = event => {
+        props.editBagel(props.id)
+    }
     return(
         <div className="bagel-listing">
             <p>{props.name}: {props.rating} </p>
             {/* <i className="fa fa-times-circle" onClick={deleteBagel}>delete</i> */}
-            <img src="https://cdn.iconscout.com/icon/premium/png-512-thumb/delete-1432400-1211078.png" alt="trashcan to delete bagel" onClick={deleteBagel} />
+                <span>
+                    <img 
+                        src="https://i.pinimg.com/originals/6e/35/ef/6e35ef7687065eb1e4c037781f3c4cdc.png" 
+                        alt="edit bagel" 
+                        onClick={editBagel} 
+                    />
+                    <img 
+                        src="https://cdn.iconscout.com/icon/premium/png-512-thumb/delete-1432400-1211078.png" 
+                        alt="trashcan to delete bagel" 
+                        onClick={deleteBagel} 
+                    />
+                </span>
         </div>
     )
 }
